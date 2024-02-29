2a438b127de7f5d5cd38e5e23e98fe6640ffdb40
aayoshh <159527498+aayoshh@users.noreply.github.com>
2024-02-29 13:16:45 +0530

    gallon.cpp

---
+++ gallon.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    float gallon;
+    double cufeet;
+    cout<<"Enter the value of gallon : ";
+    cin>>gallon;
+    cufeet = gallon/7.481;
+    cout<<cufeet;
+    return 0;
+}
